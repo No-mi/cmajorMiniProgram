483a80447bc951fe9aa26e41d381105516f5ad6c
Nomi <1150774341@qq.com>
2020-11-21 17:39:50 +0800

    添加删除和更新学生信息接口

--- model/modelDB.py
+++ model/modelDB.py
@@ -3,17 +3,17 @@ from model.DBUtil import db
 
 
 class StudentUser(db.Model):
-    username = db.Column(db.String(20), unique=False,primary_key = True)
-    student_id=db.Column(db.String(13),unique=True)
-    application_id=db.Column(db.Integer(),unique=True)
+    username = db.Column(db.String(20), unique=False, primary_key=True)
+    student_id = db.Column(db.String(13), unique=True)
+    application_id = db.Column(db.Integer(), unique=True)
 
-    __tablename__ = 'student_user'#指定对应数据库表student_user
+    __tablename__ = 'student_user'  # 指定对应数据库表student_user
 
-    def __init__(self,username,student_id,application_id=None):
-        '''初始化StudentUser'''
-        self.username=username
-        self.student_id=student_id
-        self.application_id=application_id
+    def __init__(self, username, student_id, application_id=None):
+        """初始化StudentUser"""
+        self.username = username
+        self.student_id = student_id
+        self.application_id = application_id
 
     def __repr__(self):
         return '<User %r>' % (self.username)
@@ -24,8 +24,3 @@ class StudentUser(db.Model):
         if "_sa_instance_state" in item:
             del item["_sa_instance_state"]
         return item
-
-
-
-
-
--- model/studentDB.py
+++ model/studentDB.py
@@ -1,27 +1,32 @@
 from .modelDB import *
 
+
 def getStudentUserByUserName(username):
     """根据姓名查询学生信息"""
-    user=StudentUser.query.filter_by(username=username).first()
+    user = StudentUser.query.filter_by(username=username).first()
     print(user)
     return user
+
 
 def getStudentUserByStudentID(student_id):
     """根据学号查找学生信息"""
-    user=StudentUser.query.filter_by(student_id=student_id).first()
+    user = StudentUser.query.filter_by(student_id=student_id).first()
     print(user)
     return user
 
-def insertStudent(username,student_id):
+
+def insertStudent(username, student_id):
     """插入一个学生用户"""
-    user=StudentUser(username=username,student_id=student_id)
+    user = StudentUser(username=username, student_id=student_id)
     db.session.add(user)
     db.session.commit()
+
 
 def deleteStudent(student_id):
     """删除一个学生用户"""
     StudentUser.query.filter_by(student_id=student_id).delete()
 
-def updateStudentInfo(student_id,username):
+
+def updateStudentInfo(student_id, username):
     """修改指定学号学生用户名"""
-    StudentUser.query.filter_by(student_id=student_id).update({'username':username})
+    StudentUser.query.filter_by(student_id=student_id).update({'username': username})
