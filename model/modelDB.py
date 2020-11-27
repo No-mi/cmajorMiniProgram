7a64a578270dc898f5de6d1d2e557634ddc72b33
DryPilgrim <782413632@qq.com>
2020-11-27 15:12:11 +0800

    增加Application类

--- model/modelDB.py
+++ model/modelDB.py
@@ -47,3 +47,34 @@ class StudentCourse(db.Model):
         if "_sa_instance_state" in item:
             del item["_sa_instance_state"]
         return item
+class Application(db.Model):
+    __tablename__ = 'application'  # 指定对应数据库表application
+    openID=db.Column(db.VARCHAR(40),primary_key=True)
+    name=db.Column(db.VARCHAR(20))
+    studentID=db.Column(db.VARCHAR(13))
+    institute=db.Column(db.VARCHAR(20))
+    major=db.Column(db.VARCHAR(20))
+    downGrade=db.Column(db.Integer)
+    choiceAfterGraduating=db.Column(db.Integer)
+    doctor=db.Column(db.Integer)
+    ID=db.Column(db.VARCHAR(18))
+
+    def __init__(self, openID,name,studentID,institute,major,downGrade,choiceAfterGraduating,doctor,ID):
+        """初始化application"""
+        self.openID=openID
+        self.name=name
+        self.studentID=studentID
+        self.institute=institute
+        self.major=major
+        self.downGrade=downGrade
+        self.choiceAfterGraduating=choiceAfterGraduating
+        self.doctor=doctor
+        self.ID=ID
+    def __repr__(self):
+        return '<openID %r>' % self.openID
+    def to_json(self):
+        """将实例对象转化为json"""
+        item = self.__dict__
+        if "_sa_instance_state" in item:
+            del item["_sa_instance_state"]
+        return item
