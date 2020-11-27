54e7ade2950ac1d0258694f0809f2ce25c2ddb16
DryPilgrim <782413632@qq.com>
2020-11-27 16:25:31 +0800

    申请表Application的增删改查函数接口

---
+++ model/applicationDB.py
@@ -0,0 +1,25 @@
+# from model.modelDB import Application
+from sqlalchemy import or_
+from .modelDB import *
+
+def getApplicationByIdName(name,openID,studentId):
+    """根据姓名/微信编号/学号查询申请表信息"""
+    application = Application.query.filter_by(or_(studentId==studentId , name==name ,openID==openID))
+    # return list(map(lambda x:x.cId,application))
+    return application
+
+def insertApplicqtion(openID,name,studentID,institute,major,downGrade,choiceAfterGraduating,doctor,ID):
+    """插入一个申请表信息"""
+    application = Application(openID=openID,name=name,studentID=studentID,institute=institute,
+                              major=major,downGrade=downGrade,
+                              choiceAfterGraduating=choiceAfterGraduating,doctor=doctor,ID=ID)
+    db.session.add(application)
+    db.session.commit()
+
+def deleteApplication(name,openID,studentId):
+    """根据姓名/微信编号/学号删除一个申请表信息"""
+    Application.query.filter_by(or_(openID==openID,name=name,studentId=studentId)).delete()
+
+def updateApplication(name,studentId):
+    """修改指定姓名用户的姓名"""
+    Application.query.filter_by(studentId=studentId).update({'name': name,})
