642603ec99d43862cb8c52f7f889569dd87576fc
DryPilgrim <782413632@qq.com>
2020-11-27 10:04:58 +0800

    增加类

--- model/modelDB.py
+++ model/modelDB.py
@@ -47,23 +47,3 @@ class TeacherUser(db.Model):
             del item["_sa_instance_state"]
         return item
 
-
-class Notice(db.Model):
-    __tablename__='notices' # 指定对应数据库表notices
-    notice_id=db.Column(db.Integer,primary_key=True)
-    notice_title=db.Column(db.String(100),unique=True)
-    notice_content=db.Column(db.Text,unique=True)
-    def __repr__(self):
-        return '<User %r>' % self.notice_title
-
-    #初始化
-    def __init__(self,notice_id,notice_title):
-        self.teacher_id=notice_id
-        self.name=notice_title
-
-    def to_json(self):
-        """将实例对象转化为json"""
-        item = self.__dict__
-        if "_sa_instance_state" in item:
-            del item["_sa_instance_state"]
-        return item
